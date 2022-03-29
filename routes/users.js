a714c1a72bfb4f1ab74f37e70164c8088488907a
ayoub alm <alayoub33@gmail.com>
2022-03-29 03:04:44 +0200

    add prisma function

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+const User = require('../modules/users_module')
+const { PrismaClient } = require( '@prisma/client')
+const prisma = new PrismaClient();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
 
-  res.send('route for utilisateurs');
+
+
+router.get('/all', function(req, res, next) {
+    user= new User();
+    user.getUsers(req,res);
 });
 
-router.get('/:id', function(req, res, next) {
-  res.json({
-    "user_id": req.params.id
-  });
-});
+router.post('/add', function(req, res) {
 
-
-router.post('/add', function(req, res, next) {
-
-  res.send('utilisateur bien ajouté');
+           user = new User ();
+           user.createUsers(req,res);
 });
 
 
@@ -28,5 +27,12 @@ router.patch('/update/:id', function(req,res,next){
 router.delete('/delet/:id', function(req,res,next){
 
   res.send(' l\'utlisateur est bien supprimé ');
+});
+
+
+
+router.get('/:id', async (req, res,)=> {
+  user = new User();
+  user.getUser(req,res)
 });
 module.exports = router;
